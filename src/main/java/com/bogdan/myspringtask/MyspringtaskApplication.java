2bad8567fb827d9af54ef3635b66dfce86b244db
VeratsennikauB <VeratsennikauB@app.com>
2019-04-16 14:28:50 +0300

    empty project

---
+++ src/main/java/com/bogdan/myspringtask/MyspringtaskApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.myspringtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringtaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bogdan/myspringtask/MyspringtaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bogdan.myspringtask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyspringtaskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
