29fbefb1f521b54b2786323d4d0b351159b1b6a9
VBogdan <VBogdan@app.com>
2019-04-19 17:18:44 +0300

    flayway

---
+++ src/main/resources/db/migration/V1__Init_db.sql
@@ -0,0 +1,30 @@
+create table brands (
+id bigint(20) not null auto_increment,
+name varchar(255),
+primary key (id));
+
+create table car_models (
+id bigint(20) not null auto_increment,
+name varchar(255),
+brand_id bigint(20),
+primary key (id));
+
+create table roles (
+id bigint(20) not null auto_increment,
+name varchar(255),
+primary key (id));
+
+create table users (
+id bigint(20) not null auto_increment,
+name varchar(255),
+password varchar(255),
+primary key (id));
+
+create table users_roles (
+user_id bigint(20) not null,
+role_id bigint(20) not null,
+primary key (user_id, role_id));
+
+alter table car_models add constraint car_models_brand_fk foreign key (brand_id) references brands (id);
+alter table users_roles add constraint users_roles_role_fk foreign key (role_id) references roles (id);
+alter table users_roles add constraint users_roles_user_fk foreign key (user_id) references users (id);
---
+++ src/main/resources/db/migration/V2__Add_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO brands VALUES (1,'audi'),(2,'toyota');
+
+INSERT INTO roles VALUES (1,'ADMIN'),(2,'USER');
+
+INSERT INTO users VALUES (1,'bogdan','$2a$08$ofEpeC1n32scPvYdmLY88ulogBxksvZiu/pwiCEfQmynPtbthgKue');
+
+INSERT INTO `users_roles` VALUES (1,1);
