8a1ea297f4ff7bbeccee3f5a98b4c993d80d0bb8
VBogdan <VBogdan@app.com>
2019-04-19 17:28:52 +0300

    change db2

--- src/main/resources/db/migration/V2__Add_data.sql
+++ src/main/resources/db/migration/V2__Add_data.sql
@@ -2,6 +2,6 @@ INSERT INTO brands VALUES (1,'audi'),(2,'toyota');
 
 INSERT INTO roles VALUES (1,'ADMIN'),(2,'USER');
 
-INSERT INTO users VALUES (1,'bogdan','$2a$08$ofEpeC1n32scPvYdmLY88ulogBxksvZiu/pwiCEfQmynPtbthgKue');
+INSERT INTO users VALUES (1,'root','$2a$08$ofEpeC1n32scPvYdmLY88ulogBxksvZiu/pwiCEfQmynPtbthgKue');
 
 INSERT INTO `users_roles` VALUES (1,1);
