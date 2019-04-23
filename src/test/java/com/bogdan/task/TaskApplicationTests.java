6eb27bf9b541dbf04dd5ffcb0b3fbb4713058591
VBogdan <VBogdan@app.com>
2019-04-23 16:29:45 +0300

    test fragment

--- src/test/java/com/bogdan/task/TaskApplicationTests.java
+++ src/test/java/com/bogdan/task/TaskApplicationTests.java
@@ -22,7 +22,6 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
